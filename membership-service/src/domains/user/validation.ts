caf4e792c4db6954c8a9dffb4cfb53f732c174ef
wintory <thanapol.sakul@gmail.com>
2025-04-26 19:20:43 +0700

    refactor: refactor schema

--- membership-service/src/domains/user/validation.ts
+++ membership-service/src/domains/user/validation.ts
@@ -1,6 +1,6 @@
 import Joi from 'joi'
 
-export const registerSchema = Joi.object({
+const userSchema = {
   email: Joi.string().email().required(),
   password: Joi.string().min(8).required(),
   name: Joi.string().min(2).required(),
@@ -8,16 +8,18 @@ export const registerSchema = Joi.object({
   gender: Joi.string().valid('male', 'female', 'other').required(),
   address: Joi.string().required(),
   isSubscribeNewsletter: Joi.boolean().required(),
-})
+}
+
+export const registerSchema = Joi.object(userSchema)
 
 export const editUserSchema = Joi.object({
-  dateOfBirth: Joi.date().iso().required(),
-  gender: Joi.string().valid('male', 'female', 'other').required(),
-  address: Joi.string().required(),
-  isSubscribeNewsletter: Joi.boolean().required(),
+  dateOfBirth: userSchema.dateOfBirth,
+  gender: userSchema.gender,
+  address: userSchema.address,
+  isSubscribeNewsletter: userSchema.isSubscribeNewsletter,
 })
 
 export const changePasswordSchema = Joi.object({
-  currentPassword: Joi.string().min(8).required(),
-  newPassword: Joi.string().min(8).required(),
+  currentPassword: userSchema.password,
+  newPassword: userSchema.password,
 })
