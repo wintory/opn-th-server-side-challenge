c3dc2ad1ab1d88ca5ea3ebaba3968f25fc37b253
wintory <thanapol.sakul@gmail.com>
2025-04-26 14:16:05 +0700

    build: initial rest-api project

---
+++ membership-service/app.ts
@@ -0,0 +1,11 @@
+import express, { Application } from 'express'
+
+const app: Application = express()
+
+app.use(express.json())
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
