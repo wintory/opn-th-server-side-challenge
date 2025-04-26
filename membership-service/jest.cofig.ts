31692de17ca186394d16bf01f206a66ab23195b9
wintory <thanapol.sakul@gmail.com>
2025-04-26 20:58:14 +0700

    build: initial jest

---
+++ membership-service/jest.cofig.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'jest'
+
+const config: Config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  moduleFileExtensions: ['ts', 'js', 'json'],
+  testMatch: ['**/*.test.ts'],
+  verbose: true,
+}
+
+export default config
